b4c9f2adb4b622c0e7c3f228e15ddb80490664b0
TraceyMmboneLungahi <t.lungahi@gmail.com>
2022-10-19 21:53:27 +0300

    Updated the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+puts "Seeding"
+
 Charity.create([
     {
         name: "Hope Africa",
@@ -50,3 +52,24 @@ Administrator.create([
         email: "sarahachieng@gmail.com"
     }
 ])
+
+CharityProfiles.create([
+    {
+        about_us: "We are an established charity across the coutry looking to help alleviate the various upcoming issues that school going girls face and to give them hope where there seems to be none",
+        charity_id: 1
+    },
+    {
+        about_us: "We look out for the oppressed girl child especially in remote areas and attempt as much as possible to educate her and look out for her",
+        charity_id: 2
+    }, 
+    {
+        about_us: "We sought for the girl child to enable her attain a brighter future",
+        charity_id: 3
+    },
+    {
+        about_us: "Empowering the girl child one girl at a time",
+        charity_id: 4
+    }
+])
+
+puts " Done Seeding"
