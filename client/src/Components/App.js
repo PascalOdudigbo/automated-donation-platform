27f66126dd547986522c0802cb1800b801b46087
Pascal Odudigbo <odudigbopascal@gmail.com>
2022-10-19 15:39:32 +0300

    started basic front-end setup and created HomePage

---
+++ client/src/Components/App.js
@@ -0,0 +1,14 @@
+import {Routes, Route} from "react-router-dom";
+import React,{ useState, useEffect } from "react";
+function App() {
+  return (
+   <>
+   <Routes>
+    
+   </Routes>
+   
+   </>
+  );
+}
+
+export default App;
