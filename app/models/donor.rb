721ebf3704e54c80c69f73ce3cd093a04ce215a1
TraceyMmboneLungahi <t.lungahi@gmail.com>
2022-10-19 16:21:32 +0300

    Seeded the database

--- app/models/donor.rb
+++ app/models/donor.rb
@@ -3,5 +3,5 @@ class Donor < ApplicationRecord
     has_many :charities, through: :donations
     validates :first_name, presence: true 
     validates :last_name, presence: true
-    validates :password_digest, length : { in: 8..16 }
+    validates :password_digest, length: { in: 8..16 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Charity.create([
+    {
+        name: "Hope Africa",
+        address: "Parkside Hotel, Monrovia St, City Centre, Nairobi",
+        email: "hopeafrica@gmail.com",
+        approved: true 
+    },
+    {
+        name: "Zana Africa",
+        address: "Wanguru Makutano/Embu Rd, Kerugoya, 272-10303",
+        email: "zanaafrica@gmail.com",
+        approved: true
+    },
+    {
+        name: "One Girl Can",
+        address: "Frontier Hse, Gr Flr Mosque Rd, Mandera",
+        email: "onegirlcan@gmail.com",
+        approved: true
+    },
+    {
+        name: " Women One",
+        address: "P.O. Box: 1478-60100 Embu",
+        email: "womenone@gmail.com",
+        approved: true
+    }
+])
+
+Administrator.create([
+    {
+        first_name: "John",
+        last_name: "Mwangi",
+        email: "johnmwangi@gmail.com"
+    }, 
+    {
+        first_name: "Jane",
+        last_name: "Wafula",
+        email: "janewafula@gmail.com"
+    }, 
+    {
+        first_name: "Sarah",
+        last_name: "Achieng",
+        email: "sarahachieng@gmail.com"
+    }
+])
