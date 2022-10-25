f079946274f1236721335227a9d632f76df01e25
Pascal Odudigbo <odudigbopascal@gmail.com>
2022-10-25 12:44:26 +0300

    adding functionality to keep admin logged in

--- client/src/Components/AdminDashboard.js
+++ client/src/Components/AdminDashboard.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { Routes, Route, useNavigate, redirect } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import logo from "../images/logo.png";
 import AdminCharitySearch from "./AdminCharitySearch";
 import AdminCharitiesList from "./AdminCharitiesList";
@@ -23,62 +23,53 @@ function AdminDashboard({ userData }) {
 
     useEffect(() => {
 
-        // axios.get(`http://localhost:3000/meAdministrator`)
-        //     .then(res => {
-        //         console.log(`Admin Data:`, res.data)
-        //         setUser(res.data)
-        //         console.log("First name:", user?.first_name)
-        //     })
-        //     .catch(error => {
-        //         if (error.response) {
-        //             alert(error.response.data.error);
-        //         }
-        //     })
+        fetch('/meAdministrator')
+            .then(response => response.json())
+            .then(data => {
+                setUser(data);
 
-        fetch("http://localhost:3000/meAdministrator").then((response) => {
-            if (response.ok) {
-              response.json().then((user) => setUser(user));
-            }
-          });
-       
+                if (_.isEmpty(data)) {
+                    navigate("/");
+                }
+                else {
+                    axios.get("/charities")
+                        .then(res => {
+                            //console.log(res.data);
+                            setAllCharities(res.data);
+                            handleDashboardStatistics(res.data)
+                        })
+                        .catch(error => {
+                            if (error.response) {
+                                alert(error.response.data.error);
+                            }
+                        });
+                }
 
 
-        if (_.isEmpty(user)) {
-            //navigate("/");
-        }
-        else {
-            setApproved(0);
-            setPending(0);
-            setRejected(0);
-            setTotal(0);
-            axios.get("/charities")
-                .then(res => {
-                    //console.log(res.data);
-                    setAllCharities(res.data);
-                    setTotal(total => total = res.data.length);
-                    res.data.forEach(charity => {
-                        if (charity.approved === true) {
-                            setApproved(approved => approved += 1);
+            })
+            .catch(err => console.error(err));
 
-                        } else if (charity.approved === false) {
-                            setRejected(rejected => rejected += 1);
-
-                        } else if (charity.approved === null) {
-                            setPending(pending => pending += 1)
-                        }
-
-                        console.log("total:", total, "\napproved:", approved, "\nrejected:", rejected, "\npending:", pending)
-                    })
-                })
-                .catch(error => {
-                    if (error.response) {
-                        alert(error.response.data.error);
-                    }
-                });
-        }
 
     }, [])
 
+    function handleDashboardStatistics(data){
+        setApproved(0);
+        setPending(0);
+        setRejected(0);
+        setTotal(0);
+        setTotal(total => total = data.length);
+        data.forEach(charity => {
+            if (charity.approved === true) {
+                setApproved(approved => approved += 1);
+
+            } else if (charity.approved === false) {
+                setRejected(rejected => rejected += 1);
+
+            } else if (charity.approved === null) {
+                setPending(pending => pending += 1)
+            }
+        })
+    }
 
     function handleFilteredData(searchData) {
         if (searchData === "") {
@@ -100,52 +91,31 @@ function AdminDashboard({ userData }) {
     }
 
     function handleCharityDelete(id) {
-        //console.log("called deletion");
-        //const targetAddressId = event.target.parentNode.parentNode.id;
         axios.delete(`charities/${id}`)
             .then(() => alert('Delete successful'));
 
         const newCharitiesData = allCharities.filter(charity => charity.id !== id);
         setAllCharities(newCharitiesData);
+        handleDashboardStatistics(newCharitiesData)
         setDisplayData(``);
     }
 
     function handleCharityEdit() {
-        setApproved(0);
-        setPending(0);
-        setRejected(0);
-        setTotal(0);
         axios.get("/charities")
             .then(res => {
                 console.log(res.data);
                 setAllCharities(allCharities => allCharities = res.data);
-                setTotal(total => total = res.data.length);
-                res.data.forEach(charity => {
-                    if (charity.approved === true) {
-                        setApproved(approved => approved += 1);
-
-                    } else if (charity.approved === false) {
-                        setRejected(rejected => rejected += 1);
-
-                    } else if (charity.approved === null) {
-                        setPending(pending => pending += 1)
-                    }
-
-                    console.log("total:", total, "\napproved:", approved, "\nrejected:", rejected, "\npending:", pending)
-                })
-
+                handleDashboardStatistics(res.data);
             })
             .catch(error => {
                 if (error.response) {
                     alert(error.response.data.error);
                 }
             });
-        //navigate("/admin/manage-charities");
     }
 
 
     return (
-        //NB full route path = "admin/*"
         <div className="adminDashboardContainer">
             <div className="logoAndTitle">
                 <img src={logo} alt="logo" />
@@ -158,8 +128,8 @@ function AdminDashboard({ userData }) {
                         <img className="adminImage" src="" alt="admin image" />
                     </div>
                     <div className="adminNameContainer">
-                    <label className="adminNameLabel" htmlFor="Name">Name:</label>
-                    <h3 id="Name" className="adminName">{`${user?.first_name} ${user?.last_name}`}</h3>
+                        <label className="adminNameLabel" htmlFor="Name">Name:</label>
+                        <h3 id="Name" className="adminName">{`${user?.first_name} ${user?.last_name}`}</h3>
                     </div>
                     <button className="uploadImage">Upload Image</button>
                 </div>
