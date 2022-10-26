40877f378a45beead426f7f0f9846d074c34ea0c
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-10-26 10:35:27 +0300

    updated the styling of team

--- client/src/Components/Team.js
+++ client/src/Components/Team.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from "../images/logo.png";
+import NavBar from './NavBar';
 import {
   BsInstagram,
   BsFacebook,
@@ -8,26 +9,28 @@ import {
   BsLinkedin,
 } from "react-icons/bs";
 
+
 function Team() {
   return (
     <>
-     <h1>TEAM</h1>
+    <NavBar/>
+     <h1 className='team'>TEAM</h1>
     <div className='staffContainer'>
-      <h2>LEADERSHIP</h2>
-      <p><strong>Andrew Maina</strong> <br />Founder, NIA Africa Ltd</p>
-      <p><strong>John Terry</strong> <br />Executive Director,NIA Africa Ltd,Central Africa </p>
-      <p><strong>Elvis Kibet</strong> <br />Executive Director,NIA Africa Ltd,East Africa</p>
-      <h2>STAFF</h2>
-      <p><strong>Pascal Odudigbo</strong> <br />Chief Financial Officer</p>
-      <p><strong>Tracey Lung'ahi</strong> <br />Finance & Bookkeeping</p>
-      <p><strong>Eric Muthuri</strong> <br />Office Operations Coordinator</p>
-      <p><strong>Peter Mutoi</strong> <br />Bookkeeper</p>
-      <p><strong>Dennis Kosgei</strong> <br />Researcher</p>
-      <p><strong>Daniel Kipruto</strong> <br />Office Support</p>
+      <h3 className='subHeading'>LEADERSHIP</h3>
+      <p className='paragraph'><strong>Andrew Maina</strong> <br />Founder, NIA Africa Ltd</p>
+      <p className='paragraph'><strong>John Terry</strong> <br />Executive Director,NIA Africa Ltd,Central Africa </p>
+      <p className='paragraph'><strong>Elvis Kibet</strong> <br />Executive Director,NIA Africa Ltd,East Africa</p>
+      <h3 className='subHeading'>STAFF</h3>
+      <p className='paragraph'><strong>Pascal Odudigbo</strong> <br />Chief Financial Officer</p>
+      <p className='paragraph'><strong>Tracey Lung'ahi</strong> <br />Finance & Bookkeeping</p>
+      <p className='paragraph'><strong>Eric Muthuri</strong> <br />Office Operations Coordinator</p>
+      <p className='paragraph'><strong>Peter Mutoi</strong> <br />Bookkeeper</p>
+      <p className='paragraph'><strong>Dennis Kosgei</strong> <br />Researcher</p>
+      <p className='paragraph'><strong>Daniel Kipruto</strong> <br />Office Support</p>
     </div>
 
     <div className="teamFooter">
-      <h3>follow us</h3>
+      <h4 className='follow'>follow us</h4>
       <ul>
         <li>
           <BsInstagram />
@@ -45,7 +48,7 @@ function Team() {
           <BsLinkedin />
         </li>
       </ul>
-      <h4>2022 Copyright NIA Africa Ltd</h4>
+      <h4 className='copyright'>2022 Copyright NIA Africa Ltd</h4>
     </div>
   </>
 
