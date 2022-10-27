f5b5b30cd873a64847fc85aa84ce8515d2549309
Mutoi01 <pngando9@gmail.com>
2022-10-27 11:30:27 +0300

    created beneficiary stories component

--- client/src/Components/BeneficiariesStoriesList.js
+++ client/src/Components/BeneficiariesStoriesList.js
@@ -1,40 +1,25 @@
 import React, { useState,useEffect} from "react";
-// import axios from "axios";
+import axios from "axios";
 
 function BeneficiariesStoriesList (){
-  const [allCharities, setAllCharities] = useState([]);
 
-  // const [allStories, setAllStories] = useState([]);
-
-  // useEffect(() => {
-  //   // axios.get("/stories")
-  //   axios.get("/charities")
-  //   .then(res => {
-  //       console.log(res.data);
-  //       setAllCharities(allCharities => allCharities = res.data);
-  //       // setAllStories(allStories => allStories= res.data);
-  //   })
-  //   .catch(error => {
-  //       if (error.response) {
-  //           alert(error.response.data.error);
-  //       }
-  //   });
-  // })
+  const [allStories, setAllStories] = useState([]);
 
   useEffect(() => {
-    fetch("/stories")
-      .then((response) => response.json())
-      .then((data) => {
-        setStory(data);
-        fetch(`/a_beneficiarys_stories/${data?.id}`)
-          .then((response) => response.json())
-          .then((data) => {
-            console.log("STORIES:", data);
-          })
-          .catch((err) => console.error(err));
-      })
-      .catch((err) => console.error(err));
-  }, []);
+    axios.get("/stories")
+   
+    .then(res => {
+        console.log(res.data);
+        setAllStories(allStories => allStories= res.data);
+    })
+    .catch(error => {
+        if (error.response) {
+            alert(error.response.data.error);
+        }
+    });
+  })
+
+
 
 
   return(
