e8af687f35f827de8403454f1788d48385863b09
Pascal Odudigbo <odudigbopascal@gmail.com>
2022-10-28 01:10:17 +0300

    modified story model associations and serializer to include beneficiary and inventory information

--- app/serializers/inventory_serializer.rb
+++ app/serializers/inventory_serializer.rb
@@ -2,7 +2,5 @@ class InventorySerializer < ActiveModel::Serializer
   attributes :id, :item, :quantity
   has_one :beneficiary
   has_one :charity
-  has_one :charity
-  has_one :beneficiary
   
 end
--- client/src/Components/DonorViewCharityStories.js
+++ client/src/Components/DonorViewCharityStories.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 
-function DonorViewCharityStories(){
-    
+function DonorViewCharityStories(charityData) {
+    const [charityData, setCharityData] = useState({});
+    return (
+        <div className="donorViewCharityStoryContainer">
+            <div className="navContainer">
+                <NavBar />
+            </div>
+            <h1 className="donorViewCharityStoryPageTitle">{`${charityData?.name.toUpperCase()}'s BENEFICIARY STORIES`}</h1>
+
+            <div className="donorViewCharityStoryPageCharityAboutContainer">
+                <h4 className="donorViewCharityStoryPageCharityAboutTitle">{`What's ${charityData?.name} All About`}</h4>
+
+                <p className="donorViewCharityStoryPageCharityAbout">
+                    {charityData?.charity_profile?.about_us}
+                </p>
+            </div>
+
+            <div className="charityAllStoriesContainer">
+
+            </div>
+
+        </div>
+    );
+
 }
 export default DonorViewCharityStories;
