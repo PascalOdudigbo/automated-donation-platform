63a1eb6da779f6a61ddfc792176f8c77f0d54280
Pascal Odudigbo <odudigbopascal@gmail.com>
2022-10-28 04:34:55 +0300

    finishing the charity beneficiary story view component and the styling

--- client/src/Components/DonorViewCharityStories.js
+++ client/src/Components/DonorViewCharityStories.js
@@ -1,25 +1,75 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import DonorCharityStoriesList from "./DonorCharityStoriesList";
+import NavBar from "./NavBar";
+// get our footer icon imports
+import {
+    BsInstagram,
+    BsFacebook,
+    BsGithub,
+    BsTwitter,
+    BsLinkedin,
+  } from "react-icons/bs";
+  
+let charityData = JSON.parse(localStorage.getItem("selectedCharity"));
 
+function DonorViewCharityStories() {
+    // const [charityData, setCharityData] = useState({});
+    const [allStories, setAllStories] = useState([]);
 
-function DonorViewCharityStories(charityData) {
-    const [charityData, setCharityData] = useState({});
+    useEffect(()=>{
+        charityData = {};
+        charityData = JSON.parse(localStorage.getItem("selectedCharity"));
+        fetch(`/a_charitys_stories/${charityData?.id}`)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("STORIES:", data);
+          if (!data.error) {
+            setAllStories(data);
+          }
+        })
+        .catch((err) => console.error(err));
+
+    }, [])
+
     return (
         <div className="donorViewCharityStoryContainer">
             <div className="navContainer">
                 <NavBar />
             </div>
-            <h1 className="donorViewCharityStoryPageTitle">{`${charityData?.name.toUpperCase()}'s BENEFICIARY STORIES`}</h1>
+            <h1 className="donorViewCharityStoryPageTitle">{`${charityData?.name?.toUpperCase()}'s BENEFICIARY STORIES`}</h1>
 
             <div className="donorViewCharityStoryPageCharityAboutContainer">
-                <h4 className="donorViewCharityStoryPageCharityAboutTitle">{`What's ${charityData?.name} All About`}</h4>
+                <h3 className="donorViewCharityStoryPageCharityAboutTitle">{`WHAT's ${charityData?.name?.toUpperCase()} ALL ABOUT`}</h3>
 
                 <p className="donorViewCharityStoryPageCharityAbout">
                     {charityData?.charity_profile?.about_us}
                 </p>
             </div>
 
-            <div className="charityAllStoriesContainer">
+            <div className="donorCharityAllStoriesContainer">
+                <DonorCharityStoriesList allStories={allStories}/>
+            </div>
 
+            <div className="DVCfooter">
+              <h3>follow us</h3>
+              <ul>
+                <li>
+                  <BsInstagram />
+                </li>
+                <li>
+                  <BsFacebook />
+                </li>
+                <li>
+                  <BsTwitter />
+                </li>
+                <li>
+                  <BsGithub />
+                </li>
+                <li>
+                  <BsLinkedin />
+                </li>
+              </ul>
+              <h4>2022 Copyright NIA Africa Ltd</h4>
             </div>
 
         </div>
--- client/src/Components/HomePage.js
+++ client/src/Components/HomePage.js
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from "react";
 import NavBar from "./NavBar";
 import Charity from "./Charity";
 import axios from "axios";
-import { Link } from "react-router-dom";
-//import {useNavigate} from "react-router-dom"
+import { Link, Navigate, Route, Routes } from "react-router-dom";
+import DonorViewCharityStories from "./DonorViewCharityStories";
+import { useNavigate } from "react-router-dom"
 
 // get our footer icon imports
 import {
@@ -14,10 +15,11 @@ import {
   BsLinkedin,
 } from "react-icons/bs";
 
+
 function HomePage() {
-  //const navigate = useNavigate();
+  const navigate = useNavigate();
   const [allCharities, setAllCharities] = useState([]);
-  const [selectedCharity, setSelectedCharity] = useState({});
+  // const [selectedCharity, setSelectedCharity] = useState({});
   const picturesUrlArray = [
     "https://res.cloudinary.com/devex/image/fetch/c_scale,f_auto,q_auto,w_720/https://lh6.googleusercontent.com/HtP9Lm0nws0B38i5qkyqZtwRSn8xkR8TJGkriaqiWv89mLavA-_M_V5X7iGYY93QB3JpOzrnUJ-HUWC9YaYUO1bDpoq_O8FYPj_nJFw-GC5pkMeDX418bK-4ORlbY09XVMqfIhq2",
     "http://c.files.bbci.co.uk/18FE/production/_100489360_bbc.jpg",
@@ -55,77 +57,98 @@ function HomePage() {
   }, []);
 
   function handleDonationToCharity(charity) {
-    setSelectedCharity(charity);
+    // setSelectedCharity(charity);
+    localStorage.clear();
+    localStorage.setItem("selectedCharity", JSON.stringify(charity));
     console.log("Charity being donated to:", charity);
+  }
+
+  function handleSetSelectedCharity(charity) {
+    // setSelectedCharity(charity);
+    localStorage.clear();
+    localStorage.setItem("selectedCharity", JSON.stringify(charity));
+    navigate("charity-stories")
   }
 
   return (
     <>
-      <div className="homePage">
-        <div className="navContainer">
-          <NavBar />
-        </div>
+      <Routes>
+        <Route path={""} element={
+          <div className="homePage">
+            <div className="navContainer">
+              <NavBar />
+            </div>
 
-        <img className="randomImages" src={imageUrl} alt="Charity pictures" />
+            <img className="randomImages" src={imageUrl} alt="Charity pictures" />
 
-        <p className="NiaAbout">
-          Girls from poor families miss 20 % of school days in a year due to
-          lack of sanitary towels. 2016 research data indicated that a girl in
-          primary school between class 6 and 8 can lose up to 18 weeks out of
-          108 weeks. Those in high school can lose almost 24 weeks out of 144
-          weeks of learning. It is up to us to make a change by donating to
-          charities that aim to provide sanitation facilities and materials for
-          these children. Nia Africa is an organisation dedicated to dealing
-          with this problem by providing a platform that fascilitates the
-          automation of donations to charities that support this cause. Our
-          partners pride themselves in not just providing sanitary towels but
-          also clean water and sanitation facilities such as toilets. Donate
-          today and help us meet the guidelines for proper menstrual hygiene as
-          defined by UNICEF. If you're a Charity allied to our cause and would
-          like to partner with us, please find below a link to register as a
-          charity on this platform.
-        </p>
+            <p className="NiaAbout">
+              Girls from poor families miss 20 % of school days in a year due to
+              lack of sanitary towels. 2016 research data indicated that a girl in
+              primary school between class 6 and 8 can lose up to 18 weeks out of
+              108 weeks. Those in high school can lose almost 24 weeks out of 144
+              weeks of learning. It is up to us to make a change by donating to
+              charities that aim to provide sanitation facilities and materials for
+              these children. Nia Africa is an organisation dedicated to dealing
+              with this problem by providing a platform that fascilitates the
+              automation of donations to charities that support this cause. Our
+              partners pride themselves in not just providing sanitary towels but
+              also clean water and sanitation facilities such as toilets. Donate
+              today and help us meet the guidelines for proper menstrual hygiene as
+              defined by UNICEF. If you're a Charity allied to our cause and would
+              like to partner with us, please find below a link to register as a
+              charity on this platform.
+            </p>
 
-          <Link className="registerCharity" to="/charity-register">Register Charity</Link>
-        
-        <h1 className="charitiesTitle">CHARITIES</h1>
+            <Link className="registerCharity" to="/charity-register">Register Charity</Link>
 
-        <div className="CharitiesContainer">
-          {allCharities?.map((charity) => (
-            <Charity
-              key={charity.id}
-              name={charity.name}
-              address={charity.address}
-              description={charity?.charity_profile?.about_us}
-              handleDonateClicked={() => {
-                handleDonationToCharity(charity);
-              }}
-            />
-          ))}
-        </div>
+            <h1 className="charitiesTitle">CHARITIES</h1>
 
-        <div className="footer">
-          <h3>follow us</h3>
-          <ul>
-            <li>
-              <BsInstagram />
-            </li>
-            <li>
-              <BsFacebook />
-            </li>
-            <li>
-              <BsTwitter />
-            </li>
-            <li>
-              <BsGithub />
-            </li>
-            <li>
-              <BsLinkedin />
-            </li>
-          </ul>
-          <h4>2022 Copyright NIA Africa Ltd</h4>
-        </div>
-      </div>
+            <div className="CharitiesContainer">
+              {
+                allCharities?.map((charity) => (
+                  charity?.charity_profile?.about_us ? <Charity
+                    key={charity.id}
+                    name={charity.name}
+                    address={charity.address}
+                    handleSetSelectedCharity={() => {
+                      handleSetSelectedCharity(charity);
+                    }}
+                    handleDonateClicked={() => {
+                      handleDonationToCharity(charity);
+                    }}
+                  /> : ""
+                ))
+              }
+            </div>
+
+            <div className="footer">
+              <h3>follow us</h3>
+              <ul>
+                <li>
+                  <BsInstagram />
+                </li>
+                <li>
+                  <BsFacebook />
+                </li>
+                <li>
+                  <BsTwitter />
+                </li>
+                <li>
+                  <BsGithub />
+                </li>
+                <li>
+                  <BsLinkedin />
+                </li>
+              </ul>
+              <h4>2022 Copyright NIA Africa Ltd</h4>
+            </div>
+          </div>
+        } />
+
+        <Route path={"charity-stories"} element={<DonorViewCharityStories/>} />
+
+      </Routes>
+
     </>
   );
 }
